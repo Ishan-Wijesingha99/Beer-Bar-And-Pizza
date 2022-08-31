215502297c5b8cc0e3c5e85ea1e494046ff4d817
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-31 18:24:30 +1000

    updated login page and routes

--- Develop/controllers/get-routes.js
+++ Develop/controllers/get-routes.js
@@ -9,6 +9,15 @@ const { Beer, Favourites, Ingredients, Pizza, User } = require('../models');
 router.get('/', async (req, res) => {
     res.status(200).render('homepage')
 })
+router.get('/home', async (req, res) => {
+    res.status(200).render('homepage')
+})
+
+router.get('/login', async (req, res) => {
+    res.status(200).render('login')
+})
+
+
 
 
 
--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const getRoutes = require('./get-routes.js');
+const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
 router.use('/', getRoutes);
 
 
